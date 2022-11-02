63e5f792c38e21e54206ccbf33bced92c21b0ea9
Noor Alam <alamrangrej787@gmail.com>
2022-11-03 01:53:28 +0530

    Initial commit

---
+++ JavaScript/main.js
@@ -0,0 +1,13 @@
+//Function Expressions
+// var calculateString;
+// calculateString = function (X) {
+//     console.log('Hi my name is Alam' + x)
+// }
+// console.log(calculateString)
+
+//SCOPES
+
+var x = "Noor Alam";
+function name() {
+    console.log(x)
+}
